1f38f87250368beda9833ec56478a4e6597613a8
Joe Belarge <belarge@nscl.msu.edu>
2016-05-27 16:23:42 -0400

    header file playing

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <fstream>
 
+#include "main.h"
+
 int main(int argc, char* argv[]){
 
   std::cout << "Hello" << std::endl;
+
+  if(argc!=2) return 0;
+  
+  ReadInput(argv[1]);
 
   return 0;
 }
---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <fstream>
+
+
+void ReadInput(std::string InFile){
+  std::ifstream in;
+  in.open(InFile.c_str());
+  if(in.fail()) return;
+  std::string eat;
+  while(!(in.eof())){
+    getline(in,eat);
+    std::cout << eat << std::endl;
+  }
+  
+
+}
+
+#endif 
